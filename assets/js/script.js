b8365abbab561e87cbf335cedffa79006082f198
Cade Ellsworth <caedmd2100@gmail.com>
2021-04-29 18:53:01 -0700

    ready for presentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,29 @@ var selectRange = document.querySelector("#range-select");
 var infoSubmitBtn = document.querySelector("#submit-btn");
 var infoContainerEl = document.querySelector("#info-container");
 var previousRecommendations = document.querySelector("#previous-recommendations")
-var 
 
 // create function to trigger app functions
 var initializeApp = function (e) {
     e.preventDefault();
     infoContainerEl.innerHTML = "";
-    convertAddress();
+    // convertAddress();
     getRestaurants();
 }
 
 // create function to grab address from form and convert it into latitude and longitude
-var convertAddress = function () {
-    console.log("working");
-    fetch('http://api.positionstack.com/v1/forward?access_key=ee94f47a6f59bccccb63518da296f930&query=' + userAddress.value + '&output=json')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(response);
-            // put latitude and longitude into local storage
-            localStorage.setItem("latitude", response.data[0].latitude);
-            localStorage.setItem("longitude", response.data[0].longitude);
-        });
-}
+// var convertAddress = function () {
+//     console.log("working");
+//     fetch('http://api.positionstack.com/v1/forward?access_key=ee94f47a6f59bccccb63518da296f930&query=' + userAddress.value + '&output=json')
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (response) {
+//             console.log(response);
+//             // put latitude and longitude into local storage
+//             localStorage.setItem("latitude", response.data[0].latitude);
+//             localStorage.setItem("longitude", response.data[0].longitude);
+//         });
+// }
 
 // create function to grab range + preferred cuisine, make a fetch call using range, cuisine, and lat/lon
 var getRestaurants = function () {
@@ -35,7 +34,7 @@ var getRestaurants = function () {
     var geoLat = localStorage.getItem("latitude");
     var geoLon = localStorage.getItem("longitude");
     // fetch data
-    fetch("https://api.documenu.com/v2/restaurants/search/geo?key=337e793c7092becc4d3298568d6964ac&lat=" + geoLat + "&lon=" + geoLon + "&distance=" + selectRange.value + "&fullmenu")
+    fetch("https://api.documenu.com/v2/restaurants/search/geo?key=337e793c7092becc4d3298568d6964ac&lat=33.445943&lon=-111.774751&distance=" + selectRange.value + "&fullmenu")
         .then(function (response) {
             return response.json();
         })
